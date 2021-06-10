3dfe55d461dc749f4d09a14e1a2f71f21cd49e98
fikri azhari <fikriazhari551@gmail.com>
2021-06-10 22:55:42 +0700

    bisa login

--- app/src/main/java/com/example/vaksinasishedule/LoginActivity.java
+++ app/src/main/java/com/example/vaksinasishedule/LoginActivity.java
@@ -1,32 +1,80 @@
 package com.example.vaksinasishedule;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+public class LoginActivity extends AppCompatActivity{
+
+    User usr;
+    EditText txtNama, txtPassword;
+    Button btnLogin;
+    ProgressBar progressBar;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
-        Button btn_regis = findViewById(R.id.daftarakun);
-        btn_regis.setOnClickListener(this);
+
+        txtNama = findViewById(R.id.usernameField);
+        txtPassword = findViewById(R.id.passwordField);
+        btnLogin = findViewById(R.id.masukButton);
+//        progressBar = findViewById(R.id.progressBar);
+        usr = new User();
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userLogin();
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.daftarakun:
-                Intent intet = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intet);
-                break;
+    private void userLogin(){
+        String username = txtNama.getText().toString().trim();
+        String password = txtPassword.getText().toString().trim();
 
+        if (username.isEmpty()){
+            txtNama.setError("Username Salah");
+            txtNama.requestFocus();
+            return;
         }
+        if (username.isEmpty()){
+            txtNama.setError("Password Salah");
+            txtNama.requestFocus();
+            return;
+        }
+//        progressBar.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(username, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //ets
+                            startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(), "Login Gagal", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
 }
--- app/src/main/java/com/example/vaksinasishedule/RegisterActivity.java
+++ app/src/main/java/com/example/vaksinasishedule/RegisterActivity.java
@@ -10,6 +10,10 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,23 +22,25 @@ import com.google.firebase.database.ValueEventListener;
 
 public class RegisterActivity extends AppCompatActivity {
     DatabaseReference reff;
+    FirebaseAuth mAuth;
     User usr;
-    EditText txtNama, txtPassword, txtUlangPassword,txtEmail, txtNomorTelpon;
+    EditText txtNama, txtPassword, txtUlangPassword, txtEmail, txtNomorTelpon;
     Button btnRegis;
-    long maxid=0;
+    long maxid = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_register);
-        txtNama = (EditText)findViewById(R.id.usernameField);
-        txtPassword = (EditText)findViewById(R.id.passwordField);
-        txtUlangPassword = (EditText)findViewById(R.id.ulangPasswordField);
-        txtEmail = (EditText)findViewById(R.id.emailField);
-        txtNomorTelpon = (EditText)findViewById(R.id.telponField);
+        txtNama = (EditText) findViewById(R.id.usernameField);
+        txtPassword = (EditText) findViewById(R.id.passwordField);
+        txtUlangPassword = (EditText) findViewById(R.id.ulangPasswordField);
+        txtEmail = (EditText) findViewById(R.id.emailField);
+        txtNomorTelpon = (EditText) findViewById(R.id.telponField);
         btnRegis = findViewById(R.id.BuatAkun);
         usr = new User();
+        mAuth = FirebaseAuth.getInstance();
         reff = FirebaseDatabase.getInstance().getReference().child("User");
         reff.addValueEventListener(new ValueEventListener() {
             @Override
@@ -52,17 +58,48 @@ public class RegisterActivity extends AppCompatActivity {
         btnRegis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                usr.setNama(txtNama.getText().toString().trim());
                 String password = txtPassword.getText().toString().trim();
                 String ulangPassword = txtUlangPassword.getText().toString().trim();
-                if (password.matches(ulangPassword)){
-                    usr.setKataSandi(txtPassword.getText().toString().trim());
+                if (!password.matches(ulangPassword)){
+                    Toast.makeText(getApplicationContext(),"Password Tidak Benar", Toast.LENGTH_SHORT).show();
+                    txtPassword.setText("");
+                    txtUlangPassword.setText("");
+                }else{
+                    daftarAkun();
                 }
-                usr.setEmail(txtEmail.getText().toString().trim());
-                usr.setNomorTelpon(txtNomorTelpon.getText().toString().trim());
-                reff.child(String.valueOf(maxid+1)).setValue(usr);
             }
         });
+    }
+
+    private void daftarAkun(){
+        String email = txtEmail.getText().toString().trim();
+        String password = txtPassword.getText().toString().trim();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            usr.setNama(txtNama.getText().toString().trim());
+                            usr.setEmail(txtEmail.getText().toString().trim());
+                            usr.setNomorTelpon(txtNomorTelpon.getText().toString().trim());
+                            reff.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(usr)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                Toast.makeText(getApplicationContext(), "Registrasi Berhasil", Toast.LENGTH_LONG).show();
+                                            }else{
+                                                Toast.makeText(getApplicationContext(), "Registrasi Gagal2", Toast.LENGTH_LONG).show();
+                                            }
+                                        }
+                                    });
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Registrasi Gagal1", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+        Intent intet = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(intet);
     }
 }
 
