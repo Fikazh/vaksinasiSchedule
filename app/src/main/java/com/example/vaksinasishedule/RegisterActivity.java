62e754fb3ec18bd619b094fa6f933c40e6c00da1
fikri azhari <fikriazhari551@gmail.com>
2021-06-10 09:51:18 +0700

    database daftar

--- app/src/main/java/com/example/vaksinasishedule/RegisterActivity.java
+++ app/src/main/java/com/example/vaksinasishedule/RegisterActivity.java
@@ -5,52 +5,64 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
-    private DatabaseReference mDatabase;
+public class RegisterActivity extends AppCompatActivity {
+    DatabaseReference reff;
+    User usr;
+    EditText txtNama, txtPassword, txtUlangPassword,txtEmail, txtNomorTelpon;
+    Button btnRegis;
+    long maxid=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_register);
-        Button btn_regis = findViewById(R.id.BuatAkun);
-        btn_regis.setOnClickListener(this);
+        txtNama = (EditText)findViewById(R.id.usernameField);
+        txtPassword = (EditText)findViewById(R.id.passwordField);
+        txtUlangPassword = (EditText)findViewById(R.id.ulangPasswordField);
+        txtEmail = (EditText)findViewById(R.id.emailField);
+        txtNomorTelpon = (EditText)findViewById(R.id.telponField);
+        btnRegis = findViewById(R.id.BuatAkun);
+        usr = new User();
+        reff = FirebaseDatabase.getInstance().getReference().child("User");
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists())
+                    maxid = (dataSnapshot.getChildrenCount());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        btnRegis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                usr.setNama(txtNama.getText().toString().trim());
+                String password = txtPassword.getText().toString().trim();
+                String ulangPassword = txtUlangPassword.getText().toString().trim();
+                if (password.matches(ulangPassword)){
+                    usr.setKataSandi(txtPassword.getText().toString().trim());
+                }
+                usr.setEmail(txtEmail.getText().toString().trim());
+                usr.setNomorTelpon(txtNomorTelpon.getText().toString().trim());
+                reff.child(String.valueOf(maxid+1)).setValue(usr);
+            }
+        });
     }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.BuatAkun:
-                final EditText txt = (EditText) findViewById(R.id.usernameField);
-                String name = (String) txt.getText().toString();
-                writeUser("001", name);
-                Intent intet = new Intent(RegisterActivity.this, LoginActivity.class);
-                startActivity(intet);
-                break;
-
-        }
-    }
-
-    public void writeUser(String userId, String username){
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        User usr = new User(username);
-        mDatabase.child("Users").child(userId).setValue(usr);
-    }
-
-    public class User{
-        public String name;
-        public User(){
-            this.name = null;
-        }
-        public User(String name){
-            this.name = name;
-        }
-    }
-
 }
+
---
+++ app/src/main/java/com/example/vaksinasishedule/User.java
@@ -0,0 +1,40 @@
+package com.example.vaksinasishedule;
+
+public class User {
+    public String nama, kataSandi, email, nomorTelpon;
+
+    public User() {
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getKataSandi() {
+        return kataSandi;
+    }
+
+    public void setKataSandi(String kataSandi) {
+        this.kataSandi = kataSandi;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNomorTelpon() {
+        return nomorTelpon;
+    }
+
+    public void setNomorTelpon(String nomorTelpon) {
+        this.nomorTelpon = nomorTelpon;
+    }
+}
