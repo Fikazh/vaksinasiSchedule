eb3945fe189c1b3a5bd97885dbfc7a07dcf95238
Brahma Aditama <aditama.brahma@gmail.com>
2021-06-11 21:07:59 +0700

    nyambungin layout-layout navbar

--- app/src/main/java/com/example/vaksinasishedule/DetailJadwalActivity.java
+++ app/src/main/java/com/example/vaksinasishedule/DetailJadwalActivity.java
@@ -1,8 +1,13 @@
 package com.example.vaksinasishedule;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class DetailJadwalActivity extends AppCompatActivity {
 
@@ -11,5 +16,34 @@ public class DetailJadwalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_detail_jadwal);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navbar);
+
+        bottomNavigationView.setSelectedItemId(R.id.jadwal);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.akun:
+                        startActivity(new Intent(getApplicationContext(), AkunActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.jadwal:
+                        startActivity(new Intent(getApplicationContext(), Jadwal.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.pengaturan:
+                        startActivity(new Intent(getApplicationContext(), PengaturanActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/vaksinasishedule/Jadwal.java
+++ app/src/main/java/com/example/vaksinasishedule/Jadwal.java
@@ -1,8 +1,13 @@
 package com.example.vaksinasishedule;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Jadwal extends AppCompatActivity {
 
@@ -11,5 +16,32 @@ public class Jadwal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jadwal);
         getSupportActionBar().hide();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navbar);
+
+        bottomNavigationView.setSelectedItemId(R.id.jadwal);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.akun:
+                        startActivity(new Intent(getApplicationContext(), AkunActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.jadwal:
+                        return true;
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.pengaturan:
+                        startActivity(new Intent(getApplicationContext(), PengaturanActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
