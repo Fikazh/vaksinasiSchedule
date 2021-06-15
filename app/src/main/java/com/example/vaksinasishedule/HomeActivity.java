a0aad4d7ee1036a0baf2e7cd9133c0fa8ac3e3ca
fikri azhari <fikriazhari551@gmail.com>
2021-06-15 18:18:54 +0700

    bisa block non photo di pasien

--- app/src/main/java/com/example/vaksinasishedule/HomeActivity.java
+++ app/src/main/java/com/example/vaksinasishedule/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.vaksinasishedule;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,8 +11,19 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.FirebaseError;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeActivity extends AppCompatActivity {
+    private FirebaseUser user;
+    private DatabaseReference reff;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,26 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), DataFormActivity.class));
+            }
+        });
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reff = FirebaseDatabase.getInstance().getReference();
+        userID = user.getUid();
+
+        ConstraintLayout cntDaftarPasien = findViewById(R.id.lengkapi_data);
+        reff.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.child("Pasien").hasChild(userID)){
+                    cntDaftarPasien.setVisibility(View.GONE);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
 
